85c997bfa3a71440763688406d387a5e2ef6d591
= <=>
2022-05-16 20:27:10 +0200

    authentication bug fix

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -9,20 +9,19 @@ const authenticateUser = async (err, req, res, next) => {
     }
 
     try {
-        const { name, userId, role } = isTokenValid({ token });
-        req.user = { name, userId, role };
+        const { name, id, role } = isTokenValid({ token });
+        req.user = { name, id, role };
         next()
     } catch (error) {
         throw new CustomError.UnauthenticatedError('Authentication Invalid - token is invalid');
     }
-
 
     next()
 };
 
 const authorizePermissions = (...roles) => {
     return (req, res, next) => {
-        if (!roles.includes(req.user.role)) {
+        if (!roles.includes(req.client.role)) {
             throw new CustomError.UnauthorizedError(
                 'Unauthorized access to this route'
             );
