0226a06ad3cff16c2d76de9056b22191340497c5
= <=>
2022-05-28 14:36:51 +0200

    lib bug fixes

--- db/AWSConnect.js
+++ db/AWSConnect.js
@@ -2,9 +2,9 @@ const AWS = require('aws-sdk');
 require('dotenv').config();
 
     AWS.config.update({
-        region:process.env.AWS_DEFAULT_REGION,
-        accessKeyId:process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey:process.env.AWS_SECRET_ACCESS_KEY
+        region:process.env.region,
+        accessKeyId:process.env.aws_access_key_id,
+        secretAccessKey:process.env.aws_secret_access_key
     });
 
 
